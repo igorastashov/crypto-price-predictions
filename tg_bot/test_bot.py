038483584fc5e4c00ad4241050e767d9d83f7612
Igor Astashov <astashovivl@gmail.com>
2024-02-18 15:52:08 +0700

    Added test on get_find_ticker func

--- tg_bot/test_bot.py
+++ tg_bot/test_bot.py
@@ -1,7 +1,7 @@
 import unittest
-from unittest.mock import MagicMock, AsyncMock, patch, ANY
+from unittest.mock import MagicMock, AsyncMock, patch, ANY, call
 from aiogram import types
-from app import welcome, get_name_ticker
+from app import welcome, get_name_ticker, get_find_ticker
 
 
 class TestWelcome(unittest.IsolatedAsyncioTestCase):
@@ -45,6 +45,45 @@ class TestGetNameTicker(unittest.IsolatedAsyncioTestCase):
                 "Выберите монету:",
                 reply_markup=ANY
             )
+
+
+class TestGetFindTicker(unittest.IsolatedAsyncioTestCase):
+    @patch("app.types.CallbackQuery")
+    @patch("app.FSMContext")
+    @patch("app.bot")
+    async def test_get_find_ticker_handler(self, mock_bot, mock_fsm_context, mock_callback_query):
+        selected_coin = "BTC-USD"
+        callback_instance = mock_callback_query.return_value
+        callback_instance.data = selected_coin
+        callback_instance.from_user.id = 123456789  # Пример ID пользователя
+
+        # Мокируем методы bot.answer_callback_query, bot.send_message и state.update_data
+        mock_bot.answer_callback_query = AsyncMock()
+        mock_bot.send_message = AsyncMock()
+        mock_fsm_context.update_data = AsyncMock()
+        mock_fsm_context.set_state = AsyncMock()
+
+        await get_find_ticker(callback_instance, mock_fsm_context)
+
+        # Проверяем, что функция answer_callback_query была вызвана с ожидаемыми параметрами
+        mock_bot.answer_callback_query.assert_called_once_with(callback_instance.id)
+
+        # Проверяем, что функция update_data была вызвана с ожидаемыми параметрами
+        mock_fsm_context.update_data.assert_called_once_with(coin=selected_coin)
+
+        # Проверяем, что функция set_state была вызвана с ожидаемыми параметрами
+        mock_fsm_context.set_state.assert_called_once_with(ANY)
+
+        # Проверяем, что функция send_message была вызвана с ожидаемыми параметрами
+        expected_calls = [
+            call(callback_instance.from_user.id, f"Вы выбрали {selected_coin}."),
+            call(callback_instance.from_user.id, "Введите временной интервал для построения графика в формате: "
+                                                  "YYYY-MM-DD YYYY-MM-DD (например, 2023-01-01 2023-12-31):")
+        ]
+        mock_bot.send_message.assert_has_calls(expected_calls, any_order=False)
+
+
+
 
 
 if __name__ == "__main__":
