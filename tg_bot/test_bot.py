e9e59d1b2d35e285aa669d7485bd7cd2868c4a77
Igor Astashov <astashovivl@gmail.com>
2024-02-18 14:28:54 +0700

    Added mock test on start

---
+++ tg_bot/test_bot.py
@@ -0,0 +1,60 @@
+import unittest
+from unittest.mock import MagicMock, AsyncMock, patch
+from aiogram import types
+from app import welcome, get_name_ticker
+
+
+class TestWelcome(unittest.IsolatedAsyncioTestCase):
+    @patch("app.types.Message")
+    async def test_welcome_message(self, mock_message):
+        message_instance = mock_message.return_value
+        message_instance.answer = AsyncMock()
+
+        await welcome(message_instance)
+
+        message_instance.answer.assert_called_once_with(
+            "Выбери одну из кнопок:",
+            reply_markup=types.ReplyKeyboardMarkup(
+                keyboard=[
+                    [types.KeyboardButton(text="Динамика стоимости")],
+                    [types.KeyboardButton(text="Динамика среднемесячной стоимости")],
+                    [types.KeyboardButton(text="Представление стоимости в виде торговых свечей")],
+                    [types.KeyboardButton(text="Предсказание на будущее")],
+                    [types.KeyboardButton(text="Оставить отзыв")],
+                ],
+                resize_keyboard=True,
+                input_field_placeholder="Жми!!!",
+            )
+        )
+
+
+class TestGetNameTicker(unittest.IsolatedAsyncioTestCase):
+    @patch("app.types.Message")
+    async def test_get_name_ticker_handler(self, mock_message):
+        mock_message.text = "Динамика стоимости"
+        mock_message.answer = AsyncMock()
+
+        # Создаем дополнительный мок-объект для текстового условия F.text.lower() == "динамика стоимости"
+        with patch("app.F") as mock_F:
+            mock_F.text.lower.return_value = "динамика стоимости"
+
+            await get_name_ticker(mock_message)
+
+            # Проверяем, что функция answer была вызвана с ожидаемыми параметрами
+            mock_message.answer.assert_called_once_with(
+                "Выберите монету:",
+                reply_markup=types.InlineKeyboardMarkup(
+                    inline_keyboard=[
+                        [
+                            types.InlineKeyboardButton(text="BTC-USD", callback_data="BTC-USD"),
+                        ],
+                        [
+                            types.InlineKeyboardButton(text="ETH-USD", callback_data="ETH-USD"),
+                        ]
+                    ]
+                )
+            )
+
+
+if __name__ == "__main__":
+    unittest.main()
